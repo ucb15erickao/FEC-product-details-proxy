ff35deec2ea5cc18a68ad4c04e082e6841199602
Eric Kao <ucberickao@gmail.com>
2020-10-10 19:40:44 -0700

    proxy complete

--- server/index.js
+++ server/index.js
@@ -5,50 +5,29 @@ const server = express();
 server.use(express.static(`${__dirname}/../client`));
 
 server.get('/:pid/getGalleries', (req, res) => {
-  axios.get(`http://localhost:1420/${req.params.pid}/getGalleries`)
+  axios.get(`http://ec2-54-177-71-152.us-west-1.compute.amazonaws.com/${req.params.pid}/getGalleries`)
     .then((galleries) => {
       res.status(200).send(galleries.data)
     })
     .catch(() => {
       res.status(404).send('galleries error');
+      console.log('/:pid/product-details error');
     });
 });
 
 server.get('/:pid/product-details', (req, res) => {
-    axios.get(`http://localhost:8080/${req.params.pid}/product-details`)
+    axios.get(`http://ec2-54-241-133-117.us-west-1.compute.amazonaws.com/${req.params.pid}/product-details`)
     .then((details) => {
       res.status(200).send(details.data)
     })
     .catch((error) => {
       res.status(404).send('details error');
-      console.log('/:pid/product-details error');
-    });
-});
-
-server.put('/:pid/product-details/wishlist', (req, res) => {
-  axios.put(`http://localhost:8080/${req.params.pid}/product-details/wishlist`)
-    .then((response) => {
-      res.status(200).send(response.data)
-    })
-    .catch((error) => {
-      res.status(404).send('details wishlist error');
-      console.log('/:pid/product-details/wishlist error');
-    });
-});
-
-server.get('/:pid/recommendation/getInfo', (req, res) => {
-  axios.get(`http://localhost:1234/${req.params.pid}/recommendation/getInfo`)
-    .then((recommendations) => {
-      res.status(200).send(recommendations.data)
-    })
-    .catch((error) => {
-      res.status(404).send('rec error');
-      console.log('/:pid/recommendation/getInfo error');
+      console.log('/:pid/getGalleries error');
     });
 });
 
 server.get('/:pid/reviews', (req, res) => {
-  axios.get(`http://localhost:3000/${req.params.pid}/reviews`)
+  axios.get(`http://ec2-3-101-21-222.us-west-1.compute.amazonaws.com/${req.params.pid}/reviews`)
     .then((reviewData) => {
       res.status(200).send(reviewData.data)
     })
@@ -59,13 +38,24 @@ server.get('/:pid/reviews', (req, res) => {
 });
 
 server.put('/:pid/reviews/vote/:voteType/:id/:toggle', (req, res) => {
-  axios.put(`http://localhost:3000/${req.params.pid}/reviews/vote/:voteType/:id/:toggle`)
+  axios.put(`http://ec2-3-101-21-222.us-west-1.compute.amazonaws.com/${req.params.pid}/reviews/vote/:voteType/:id/:toggle`)
     .then((reviewVote) => {
       res.status(200).send(reviewVote.data)
     })
     .catch((error) => {
       res.status(404).send('reviewVote error');
       console.log('/:pid/reviews/vote/:voteType/:id/:toggle error');
+    });
+});
+
+server.get('/:pid/recommendation/getInfo', (req, res) => {
+  axios.get(`http://ec2-13-57-228-157.us-west-1.compute.amazonaws.com/${req.params.pid}/recommendation/getInfo`)
+    .then((recommendations) => {
+      res.status(200).send(recommendations.data)
+    })
+    .catch((error) => {
+      res.status(404).send('rec error');
+      console.log('/:pid/recommendation/getInfo error');
     });
 });
 
